3641e5803f7e03f64a4c6d2fe492d5534ee9a5e2
Seyi Pythonian <prontomaster@gmail.com>
2024-12-17 02:16:40 +0100

    added error pages

--- apps/core/views.py
+++ apps/core/views.py
@@ -17,3 +17,33 @@ def dashboard(request):
         return redirect("bettor:dashboard")
     else:
         return redirect("core:home")
+
+
+##################################################
+#                   ERROR PAGES                  #
+##################################################
+
+
+def error_400(request, exception):
+    """Handle 400 Bad Request errors."""
+    return render(request, "400.html", status=400)
+
+
+def error_403(request, exception):
+    """Handle 403 Forbidden errors."""
+    return render(request, "403.html", status=403)
+
+
+def error_405(request, exception):
+    """Handle 405 Method Not Allowed errors."""
+    return render(request, "405.html", status=405)
+
+
+def error_404(request, exception):
+    """Handle 404 Not Found errors."""
+    return render(request, "404.html", status=404)
+
+
+def error_500(request):
+    """Handle 500 Internal Server Error errors."""
+    return render(request, "500.html", status=500)
